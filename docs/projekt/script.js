a40258482b0f1e2332ca0c9311b7a52de7f698ed
zzapala <zzapala@student.agh.edu.pl>
2025-10-22 14:21:09 +0200

    rec_page added

--- docs/projekt/script.js
+++ docs/projekt/script.js
@@ -3,3 +3,19 @@ fetch("header.html")
   .then(data => {
     document.getElementById("header-placeholder").innerHTML = data;
   });
+
+  function filterProjects(category) {
+    document.querySelectorAll('.filter-btn').forEach(btn => {
+        btn.classList.remove('active');
+    });
+    event.target.classList.add('active');
+    
+    const projects = document.querySelectorAll('.movie_card');
+    projects.forEach(project => {
+        if (category === 'all' || project.getAttribute('data-category') === category) {
+            project.classList.remove('hidden');
+        } else {
+            project.classList.add('hidden');
+        }
+    });
+}
